702f2727e59fe1c1f317a9707d2f5bd8af1d6a8d
Emma Lee <jy.lee.emma@gmail.com>
2023-02-07 14:20:04 -0800

    Implement unit tests to is_sorted(list)

--- test_is_sorted.py
+++ test_is_sorted.py
@@ -1,12 +1,8 @@
 from unittest import TestCase
 import sorted
 
+
 class TestIsSorted(TestCase):
-    def test_is_sorted_zero(self):
-        actualValue = sorted.is_sorted([0, 0, 0])
-        expectedValue = True
-        message = "Test failed"
-        self.assertEqual(expectedValue, actualValue, message)
 
     def test_is_sorted_positive_sorted(self):
         actualValue = sorted.is_sorted([3, 4, 5])
@@ -14,14 +10,38 @@ class TestIsSorted(TestCase):
         message = "Test failed"
         self.assertEqual(expectedValue, actualValue, message)
 
-    def test_is_sorted_positive_unsorted(self):
-        actualValue = sorted.is_sorted([5, 1, 3])
+    def test_is_sorted_negative_unsorted(self):
+        actualValue = sorted.is_sorted([-5, -6, -3])
         expectedValue = False
         message = "Test failed"
         self.assertEqual(expectedValue, actualValue, message)
 
-    def test_is_sorted_negative_unsorted(self):
-        actualValue = sorted.is_sorted([-5, -6, -3])
+    def test_is_sorted_positive_and_negative_unsorted(self):
+        actualValue = sorted.is_sorted([-5, 5, -3])
+        expectedValue = False
+        message = "Test failed"
+        self.assertEqual(expectedValue, actualValue, message)
+
+    def test_is_sorted_positive_and_negative_sorted(self):
+        actualValue = sorted.is_sorted([-5, -4, 3])
+        expectedValue = True
+        message = "Test failed"
+        self.assertEqual(expectedValue, actualValue, message)
+
+    def test_is_sorted_empty_list(self):
+        actualValue = sorted.is_sorted([])
+        expectedValue = True
+        message = "Test failed"
+        self.assertEqual(expectedValue, actualValue, message)
+
+    def test_is_sorted_one_element_list(self):
+        actualValue = sorted.is_sorted([1])
+        expectedValue = True
+        message = "Test failed"
+        self.assertEqual(expectedValue, actualValue, message)
+
+    def test_is_sorted_several_elements_list(self):
+        actualValue = sorted.is_sorted([-5, -4, 3, 6, -9])
         expectedValue = False
         message = "Test failed"
         self.assertEqual(expectedValue, actualValue, message)
